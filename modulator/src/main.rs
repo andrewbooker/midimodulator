eece64752082e92c8ebb4452004a06a3a10309f6
Andrew Booker <improvizone@gmail.com>
2021-01-06 19:06:25 +0000

    outputting values roughly every 10th of a second

--- modulator/src/main.rs
+++ modulator/src/main.rs
@@ -1,11 +1,24 @@
-use std::f32;
+use std::{
+    f32,
+    thread,
+    time::{Duration, Instant}
+};
+
+fn output_from(start: Instant) {
+    let freq = 0.8;
+
+    let dt = start.elapsed().as_millis() as f32;
+    let ang_freq =  freq * 2.0 * f32::consts::PI as f32;
+
+    println!("{} {}", dt, (dt * 0.001 * ang_freq).cos());
+}
 
 fn main() {
-    let max_updates_per_second = 10;
-    let freq = 0.8;
-    let ang_freq = freq * 2.0 * f32::consts::PI / max_updates_per_second as f32;
+    let start = Instant::now();
 
-    for i in 0..max_updates_per_second {
-        println!("{} {}", i, (i as f32 * ang_freq).cos());
+    for i in 0..10 {
+        output_from(start);
+        thread::sleep(Duration::from_millis(100));
     }
+
 }
