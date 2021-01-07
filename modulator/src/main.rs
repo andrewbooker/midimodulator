e13f9166c2909c8768b74f3bf2e416ee3ec9c80f
Andrew Booker <improvizone@gmail.com>
2021-01-07 11:06:18 +0000

    rounding values to nearest integer

--- modulator/src/main.rs
+++ modulator/src/main.rs
@@ -5,14 +5,15 @@ use std::{
 };
 
 fn output_from(start: Instant) {
-    let freq = 0.8;
+    let freq_hz = 0.05;
     let max_val: f32 = 40 as f32;
     let min_val: f32 = -50 as f32;
 
     let dt = start.elapsed().as_millis() as f32;
-    let ang_freq = freq * 2.0 * f32::consts::PI as f32;
+    let ang_freq = freq_hz * 2.0 * f32::consts::PI as f32;
 
-    println!("{} {}", dt, min_val + ((max_val - min_val) * 0.5 * (1.0 + (dt * 0.001 * ang_freq).cos())));
+    let val = min_val + ((max_val - min_val) * 0.5 * (1.0 + (dt * 0.001 * ang_freq).cos()));
+    println!("{} {}", dt, val.round() as i32);
 }
 
 fn main() {
