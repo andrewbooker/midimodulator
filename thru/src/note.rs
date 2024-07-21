12c85caad9c4ddd8d38741ed3168014499af5563
Andrew Booker <improvizone@gmail.com>
2024-07-21 15:18:18 +0100

    adding Scale test and fixing note access issue

--- thru/src/note.rs
+++ thru/src/note.rs
@@ -50,10 +50,11 @@ impl NoteStats {
     }
 }
 
+
 // Scale
 
 pub type Mode = [u8; 6];
-const SCALE_LENGTH: usize = 9;
+const SCALE_LENGTH: usize = 8;
 
 pub struct Scale {
     pub notes: [u8; SCALE_LENGTH]
@@ -83,10 +84,35 @@ impl Scale {
     }
 
     pub fn at(&self, idx: u8) -> u8 {
-        let i = idx as usize;
-        if self.notes.len() > i {
-            return self.notes[i];
+        let len = self.notes.len() as u8;
+        if len > idx {
+            return self.notes[idx as usize];
         }
-        0
+        return self.at(idx + 1 - len) + 12;
+    }
+}
+
+
+#[cfg(test)]
+mod tests_scale {
+    use super::Scale;
+
+    #[test]
+    fn can_construct_from_tonic_and_mode() {
+        let scale = Scale::from(60, &[2, 2, 1, 2, 2, 2]);
+
+        assert_eq!(scale.notes, [60, 62, 64, 65, 67, 69, 71, 72]);
+        assert_eq!(scale.at(0), 60);
+    }
+
+    #[test]
+    fn can_access_scale_element_beyond_octave() {
+        let scale = Scale::from(60, &[2, 2, 1, 2, 2, 2]);
+
+        assert_eq!(scale.at(0), 60);
+        assert_eq!(scale.at(7), 72);
+        assert_eq!(scale.at(8), 74);
+        assert_eq!(scale.at(9), 76);
+        assert_eq!(scale.at(10), 77);
     }
 }
