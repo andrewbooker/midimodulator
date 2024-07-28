a8dbd8d0f6769e68d63a4bee01a79507a1623321
Andrew Booker <improvizone@gmail.com>
2024-07-28 22:43:45 +0100

    tweak to console print

--- modulator/src/main.rs
+++ modulator/src/main.rs
@@ -229,7 +229,7 @@ fn main() {
 
     let ports = serialport::available_ports().expect("No ports found!");
     for p in ports {
-        println!("{}", p.port_name);
+        println!("{} available", p.port_name);
     }
 
     let (cmd_dump_tx, cmd_dump_rx) = mpsc::channel();
